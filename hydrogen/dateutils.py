82ee84d1268ff162008c90508b250483ef43eda3
yowtzu <yowtzu.lim@gmail.com>
2017-02-12 23:34:21 +0000

    Some date constants added

--- hydrogen/dateutils.py
+++ hydrogen/dateutils.py
@@ -1,4 +1,21 @@
+import numpy as np
 import pandas as pd
+
+
+'''' Some date constants '''
+
+N_DAYS_IN_YEAR = 365.25
+
+N_BDAYS_IN_YEAR = 256.0
+ROOT_N_BDAYS_IN_YEAR = np.sqrt(N_BDAYS_IN_YEAR)
+
+N_WEEKS_IN_YEAR = N_DAYS_IN_YEAR / 7.0
+ROOT_N_WEEKS_IN_YEAR = np.sqrt(N_WEEKS_IN_YEAR)
+
+N_MONTHS_IN_YEAR = 12.0
+ROOT_N_MONTHS_IN_YEAR = np.sqrt(N_MONTHS_IN_YEAR)
+
+EPOCH=pd.datetime(2000,1,1)
 
 
 def nearest_date_after(dates: pd.Series, pivot_date: pd.tslib.Timestamp):
