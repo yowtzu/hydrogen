Fix vol calculation Add target absolute for forecast

hydrogen/analytics.py
@@ -6,9 +6,9 @@ import hydrogen.system as system
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-def vol(price_df: pd.DataFrame, price_unit, annualised, method: str = 'YZ', **kwargs) -> pd.TimeSeries:
+def vol(price_df: pd.DataFrame, annualised, method: str = 'YZ', **kwargs) -> pd.TimeSeries:
     """
-
+    
     Args:
         price_df: OHLC(V) Price Data Frame with regular interval
         calc: The type of volatility calculation. At the moment, it supports SD, EWMA or ATR
@@ -24,9 +24,6 @@ def vol(price_df: pd.DataFrame, price_unit, annualised, method: str = 'YZ', **kw
         'RS': _vol_rs,
         'YZ': _vol_yz
     }.get(method)(price_df, **kwargs)
-
-    if price_unit:
-        res *= price_df.CLOSE
 
     if annualised:
         res *= system.root_n_bday_in_year
@@ -77,6 +74,8 @@ def _vol_yz(price_df: pd.DataFrame, ewm=False, **kwargs):
     price_df["CLOSE_PREV"] = price_df.CLOSE.shift(1)
 
     rs_vol = _vol_rs(price_df, ewm, **kwargs)
+    #rs_vol = _vol_rs(price_df, ewm, window=66)
+
     rs_var = rs_vol * rs_vol
 
     if ewm:
@@ -88,85 +87,11 @@ def _vol_yz(price_df: pd.DataFrame, ewm=False, **kwargs):
         open_close_var = np.log(price_df.CLOSE / price_df.OPEN).rolling(**kwargs).var()
         window = kwargs['window']
 
+        #overnight_var = np.log(price_df.OPEN / price_df.CLOSE_PREV).rolling(window=66).var()
+        #pen_close_var = np.log(price_df.CLOSE / price_df.OPEN).rolling(window=66).var()
+        #window = 66
+
     k = 0.34 / (1.34 + (window + 1) / (window - 1))
 
-    return np.sqrt(overnight_var + k * open_close_var + (1 - k) * rs_var)
-
-def summary(df: pd.DataFrame, **kwargs):
-    stats = df.describe()
-    median = df.median()
-    median.name = 'median'
-
-    skew = df.skew()
-    skew.name = 'skew'
-
-    kurtosis = df.kurtosis()
-    kurtosis.name = 'kurtosis'
-
-    corr = df.corr()
-
-    return stats.append(median).append(skew).append(kurtosis), corr
-
-def to_usd(price:pd.DataFrame, ccy:str):
-    # TODO
-    return price
-
-#####
-#
-# BACK_TESTED_SR_DAILY = 1.0
-#
-#
-# def forecast_to_position(inst: Instrument,
-#                          combined_forecast: pd.DataFrame,
-#                          trading_capital=10000000,
-#                          vol_target_pct=BACK_TESTED_SR_DAILY / 4,
-#                          average_abs_forecast=10
-#                          ):
-#     vol_target_cash_annual = trading_capital * vol_target_pct
-#     daily_vol_target_cash_daily = vol_target_cash_annual / 16
-#
-#     volatility_scalar = daily_vol_target_cash_daily / inst.instrument_value_vol
-#
-#     return combined_forecast * volatility_scalar / average_abs_forecast
-#
-#
-# def pnl_single_forecast(inst: Instrument,
-#                         forecast: pd.DataFrame):
-#     ''' forecast is single column,
-#         but always return signle column dataframe
-#     '''
-#     position = forecast_to_position(inst, forecast)
-#
-#     return inst.diff * inst.fx * position
-#
-#
-# def combine_forecast(forecast: pd.DataFrame):
-#     if len(forecast.columns) == 1:
-#         return 1, 1, forecast
-#     else:
-#         combined_forecast = pd.DataFrame()
-#         pnl = forecast.apply(pnl_single_forecast, axis='column')
-#         # portfolio optimisation
-#         weight = None  # portfolio_opt(pnl)
-#
-#         scaling_factor = 1 / weight.dot(pnl).weight
-#         scaled_combined_forecast = scaling_factor * weight * scaling_factor
-#         return weight, scaling_factor, scaled_combined_forecast
-#
-#
-# def pnl_multiple_forecast(inst: Instrument,
-#                           forecast: pd.DataFrame):
-#     ''' forecast can be multiple columns,
-#         but always return signle column dataframe
-#     '''
-#     _, _, forecast = combine_forecast(forecast)
-#     positions = forecast_to_position(inst, forecast)
-#
-#     return inst.diff * inst.fx * positions
-#
-#
-# def position_to_portfolio(inst_list, combined_forecast_list):
-#     portfolio = pd.DataFrame()
-#     # optimisation
-#     # portfolio optimisation again
-#     return portfolio
+    res =  np.sqrt(overnight_var + k * open_close_var + (1 - k) * rs_var)
+    return res